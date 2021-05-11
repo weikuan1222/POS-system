93668fead1af3ace358cde3a9b7a59df86400298
weikuan1222 <83998638+weikuan1222@users.noreply.github.com>
2021-05-11 12:08:49 +0800

    Add files via upload

---
+++ POS/pos/pos/Form3.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Data.OleDb
+Public Class Form3
+    Dim command As New OleDbDataAdapter
+    Dim dt As New DataTable
+    Dim con As New OleDbConnection
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\usr\Documents\test2.accdb"
+        con.Open()
+        command = New OleDbDataAdapter("SELECT * FROM tblReport", con)
+        command.Fill(dt)
+        DataGridView1.DataSource = dt.DefaultView
+        con.Close()
+
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Me.Hide()
+        Form2.Show()
+
+    End Sub
+End Class

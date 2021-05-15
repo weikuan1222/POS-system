b41f6031c7ff690f40a88cdc933c579964fe5756
weikuan1222 <83998638+weikuan1222@users.noreply.github.com>
2021-05-15 13:05:30 +0800

    update

--- POS/pos/pos/Form/Operation.vb
+++ POS/pos/pos/Form/Operation.vb
@@ -10,34 +10,6 @@ Public Class Operation
     Inherits System.Windows.Forms.Form
 
     Public Property loginuser As String
-
-    Public Const GENERIC_WRITE = &H40000000
-    Public Const OPEN_EXISTING = 3
-    Public Const FILE_SHARE_WRITE = &H2
-
-
-    Dim LPTPORT As String
-    Dim hPort As Integer
-
-    Public Declare Function CreateFile Lib "kernel32" Alias "CreateFileA" (
-    ByVal lpFileName As String, ByVal dwDesiredAccess As Integer,
-    ByVal dwShareMode As Integer,
-    <MarshalAs(UnmanagedType.Struct)> ByRef lpSecurityAttributes As SECURITY_ATTRIBUTES,
-    ByVal dwCreationDisposition As Integer, ByVal dwFlagsAndAttributes As Integer,
-    ByVal hTemplateFile As Integer) As Integer
-
-    Public Declare Function CloseHandle Lib "kernel32" Alias "CloseHandle" (ByVal hObject As Integer) As Integer
-    Dim retval As Integer
-    <StructLayout(LayoutKind.Sequential)> Public Structure SECURITY_ATTRIBUTES
-        Private nLength As Integer
-        Private lpSecurityDescriptor As Integer
-        Private bInheritHandle As Integer
-    End Structure
-
-
-
-
-
     Dim StoreName As String = "RECEIPT"
 
     'for item sales | untuk item penjualan
@@ -64,19 +36,6 @@ Public Class Operation
         ItemRow("amount") = txtAmount.Text
         ItemRow("remark") = txtRemark.Text
         dtItem.Rows.Add(ItemRow)
-    End Sub
-
-    Public Sub CashDrawer()
-        Dim intFileNo As Integer = FreeFile()
-        FileOpen(1, "c:\escapes.txt", OpenMode.Output)
-        PrintLine(1, Chr(27) & Chr(112) & Chr(0) & Chr(25) & Chr(250))
-        FileClose(1)
-        Shell("print /d:LPT1 c:\escapes.txt", vbNormalFocus)
-    End Sub
-
-    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-
     End Sub
 
     Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
@@ -121,7 +80,6 @@ Public Class Operation
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
         Data_Load()
 
         Printer.NewPrint()
@@ -136,28 +94,19 @@ Public Class Operation
 
         Printer.Print(" ") 'spacing
         Printer.SetFont("Courier New", 8, FontStyle.Bold) 'Setting Font
-        arrWidth = {60, 50, 50} 'array for column width | array untuk lebar kolom
-        arrFormat = {c.MidLeft, c.MidRight, c.MidRight} 'array alignment 
-        arrFormat = {c.MidLeft, c.MidRight, c.MidRight} 'array alignment 
-        arrFormat = {c.MidLeft, c.MidRight, c.MidRight} 'array alignment 
-        arrFormat = {c.MidLeft, c.MidRight, c.MidRight} 'array alignment 
-        arrFormat = {c.MidLeft, c.MidRight, c.MidRight} 'array alignment 
+        arrWidth = {130, 120} 'array for column width | array untuk lebar kolom
+        arrFormat = {c.MidLeft, c.MidRight} 'array alignment 
 
-        'column header split by ; | nama kolom dipisah dengan ;
-        Printer.Print("bill;amount;remark", arrWidth, arrFormat)
-        Printer.SetFont("Courier New", 8, FontStyle.Regular) 'Setting Font
-        Printer.Print("------------------------------------") 'line
-
-        For r = 0 To dtItem.Rows.Count - 1
-            Printer.Print(dtItem.Rows(r).Item("bill") & ";" & dtItem.Rows(r).Item("amount") & ";" & dtItem.Rows(r).Item("remark"))
-        Next
+        Printer.Print("Bill No." & txtBill.Text, arrWidth, arrFormat)
+        Printer.Print("Amount:" & txtAmount.Text, arrWidth, arrFormat)
+        Printer.Print("Remark:" & txtRemark.Text, arrWidth, arrFormat)
 
         Printer.Print("------------------------------------")
         arrWidth = {130, 120} 'array for column width | array untuk lebar kolom
         arrFormat = {c.MidLeft, c.MidRight} 'array alignment 
 
-        Printer.Print("Cashier;" & txtUser.Text, arrWidth, arrFormat)
-        Printer.Print("Date;" & txtDate.Text, arrWidth, arrFormat)
+        Printer.Print("Cashier:" & txtUser.Text, arrWidth, arrFormat)
+        Printer.Print("Date:" & txtDate.Text, arrWidth, arrFormat)
 
         'Release the job for actual printing
         Printer.DoPrint()
